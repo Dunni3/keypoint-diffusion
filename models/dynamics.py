bbd646e6475a7beb804c159c881fd115505d1b61
Ian Dunn <ian@iandunn.io>
2022-12-01 16:52:00 -0500

    implemented hetero graph conv for lig/rec complex

--- models/dynamics.py
+++ models/dynamics.py
@@ -1,59 +1,14 @@
-"""Torch Module for E(n) Equivariant Graph Convolutional Layer"""
-# pylint: disable= no-member, arguments-differ, invalid-name
 import torch
 import torch.nn as nn
-
+from typing import Dict
 import dgl.function as fn
 
 
-# this script was adapted from the DGL implementation of EGNN
-# original script: https://github.com/dmlc/dgl/blob/76bb54044eb387e9e3009bc169e93d66aa004a74/python/dgl/nn/pytorch/conv/egnnconv.py
-# I have extended this model to operate on heterogeneous graphs
-# containg receptor keypoints and ligand atoms
+class LigRecConv(nn.Module):
 
-
-class ComplexEGNNConv(nn.Module):
-    r"""Equivariant Graph Convolutional Layer from `E(n) Equivariant Graph
-    Neural Networks <https://arxiv.org/abs/2102.09844>`__
-
-    .. math::
-
-        m_{ij}=\phi_e(h_i^l, h_j^l, ||x_i^l-x_j^l||^2, a_{ij})
-
-        x_i^{l+1} = x_i^l + C\sum_{j\in\mathcal{N}(i)}(x_i^l-x_j^l)\phi_x(m_{ij})
-
-        m_i = \sum_{j\in\mathcal{N}(i)} m_{ij}
-
-        h_i^{l+1} = \phi_h(h_i^l, m_i)
-
-    where :math:`h_i`, :math:`x_i`, :math:`a_{ij}` are node features, coordinate
-    features, and edge features respectively. :math:`\phi_e`, :math:`\phi_h`, and
-    :math:`\phi_x` are two-layer MLPs. :math:`C` is a constant for normalization,
-    computed as :math:`1/|\mathcal{N}(i)|`.
-
-    Parameters
-    ----------
-    in_size : int
-        Input feature size; i.e. the size of :math:`h_i^l`.
-    hidden_size : int
-        Hidden feature size; i.e. the size of hidden layer in the two-layer MLPs in
-        :math:`\phi_e, \phi_x, \phi_h`.
-    out_size : int
-        Output feature size; i.e. the size of :math:`h_i^{l+1}`.
-    edge_feat_size : int, optional
-        Edge feature size; i.e. the size of :math:`a_{ij}`. Default: 0.
-
-    Example
-    -------
-    >>> import dgl
-    >>> import torch as th
-    >>> from dgl.nn import EGNNConv
-    >>>
-    >>> g = dgl.graph(([0,1,2,3,2,5], [1,2,3,4,0,3]))
-    >>> node_feat, coord_feat, edge_feat = th.ones(6, 10), th.ones(6, 3), th.ones(6, 2)
-    >>> conv = EGNNConv(10, 10, 10, 2)
-    >>> h, x = conv(g, node_feat, coord_feat, edge_feat)
-    """
+    # this class was adapted from the DGL implementation of EGNN
+    # original code: https://github.com/dmlc/dgl/blob/76bb54044eb387e9e3009bc169e93d66aa004a74/python/dgl/nn/pytorch/conv/egnnconv.py
+    # I have extended the EGNN graph conv layer to operate on heterogeneous graphs containing containing receptor and ligand nodes
 
     def __init__(self, in_size, hidden_size, out_size, edge_feat_size=0):
         super().__init__()
@@ -64,14 +19,21 @@ class ComplexEGNNConv(nn.Module):
         self.edge_feat_size = edge_feat_size
         act_fn = nn.SiLU()
 
-        # \phi_e
-        self.edge_mlp = nn.Sequential(
-            # +1 for the radial feature: ||x_i - x_j||^2
-            nn.Linear(in_size * 2 + edge_feat_size + 1, hidden_size),
-            act_fn,
-            nn.Linear(hidden_size, hidden_size),
-            act_fn,
-        )
+        self.edge_types = ["ll", "rl"]
+
+        # TODO: how many layers are in the MLPs of Hoogeboom/Arnescheung papers?
+        # TODO: what activation function do they use in Hoogeboom? which should i use?
+
+        # \phi_e^t
+        self.edge_mlp: Dict[str, nn.Sequential] = {}
+        for edge_type in self.edge_types:
+            self.edge_mlp[edge_type] = nn.Sequential(
+                # +1 for the radial feature: ||x_i - x_j||^2
+                nn.Linear(in_size * 2 + edge_feat_size + 1, hidden_size),
+                act_fn,
+                nn.Linear(hidden_size, hidden_size),
+                act_fn,
+            )
 
         # \phi_h
         self.node_mlp = nn.Sequential(
@@ -80,33 +42,43 @@ class ComplexEGNNConv(nn.Module):
             nn.Linear(hidden_size, out_size),
         )
 
-        # \phi_x
-        self.coord_mlp = nn.Sequential(
-            nn.Linear(hidden_size, hidden_size),
-            act_fn,
-            nn.Linear(hidden_size, 1, bias=False),
-        )
+        # \phi_x^t
+        self.coord_mlp: Dict[str, nn.Sequential] = {}
+        for edge_type in self.edge_types:
+            self.coord_mlp[edge_type] = nn.Sequential(
+                # +1 for the radial feature: ||x_i - x_j||^2
+                nn.Linear(in_size * 2 + edge_feat_size + 1, hidden_size),
+                act_fn,
+                nn.Linear(hidden_size, 1),
+                act_fn,
+            )
 
     def message(self, edges):
         """message function for EGNN"""
-        # concat features for edge mlp
+        # concat features for edge-wise operations
         if self.edge_feat_size > 0:
             f = torch.cat(
                 [
                     edges.src["h"],
                     edges.dst["h"],
-                    edges.data["radial"],
+                    edges.data["dij"],
                     edges.data["a"],
                 ],
                 dim=-1,
             )
         else:
             f = torch.cat(
-                [edges.src["h"], edges.dst["h"], edges.data["radial"]], dim=-1
+                [edges.src["h"], edges.dst["h"], edges.data["dij"]], dim=-1
             )
 
-        msg_h = self.edge_mlp(f)
-        msg_x = self.coord_mlp(msg_h) * edges.data["x_diff"]
+        # get edge type
+        edge_type = edges.canonical_etype[1]
+
+        # compute feature messages
+        msg_h = self.edge_mlp[edge_type](f)
+
+        # compute coordinate messages
+        msg_x = self.coord_mlp[edge_type](f) * edges.data["x_diff"]
 
         return {"msg_x": msg_x, "msg_h": msg_h}
 
@@ -119,56 +91,74 @@ class ComplexEGNNConv(nn.Module):
         Parameters
         ----------
         graph : DGLGraph
-            The graph.
-        node_feat : torch.Tensor
-            The input feature of shape :math:`(N, h_n)`. :math:`N` is the number of
-            nodes, and :math:`h_n` must be the same as in_size.
-        coord_feat : torch.Tensor
-            The coordinate feature of shape :math:`(N, h_x)`. :math:`N` is the
-            number of nodes, and :math:`h_x` can be any positive integer.
+            Heterogeneous graph with ligand and receptor nodes.
+        node_feat : Dict[str, torch.Tensor]
+            Ligand and receptor node features
+        coord_feat : Dict[str, torch.Tensor]
+            Ligand and receptor coordinate features
         edge_feat : torch.Tensor, optional
-            The edge feature of shape :math:`(M, h_e)`. :math:`M` is the number of
-            edges, and :math:`h_e` must be the same as edge_feat_size.
+            Edge features. Not presently implemented. 
 
         Returns
         -------
-        node_feat_out : torch.Tensor
-            The output node feature of shape :math:`(N, h_n')` where :math:`h_n'`
-            is the same as out_size.
-        coord_feat_out: torch.Tensor
-            The output coordinate feature of shape :math:`(N, h_x)` where :math:`h_x`
-            is the same as the input coordinate feature dimension.
+        node_feat_out : Dict[str, torch.Tensor]
+        coord_feat_out: Dict[str, torch.Tensor]
         """
+        assert graph.etypes == self.edge_types
         with graph.local_scope():
             # node feature
             graph.ndata["h"] = node_feat
             # coordinate feature
             graph.ndata["x"] = coord_feat
+
             # edge feature
             if self.edge_feat_size > 0:
                 assert edge_feat is not None, "Edge features must be provided."
                 graph.edata["a"] = edge_feat
-            # get coordinate diff & radial features
+                
+
+            # compute displacement vector between nodes for all edges
+            # TODO: this is u_sub_v ... should we do v_sub_u? which way is information flowing? does this 
+            # affect how I construct edges in my initial graph??
             graph.apply_edges(fn.u_sub_v("x", "x", "x_diff"))
-            graph.edata["radial"] = (
-                graph.edata["x_diff"].square().sum(dim=1).unsqueeze(-1)
-            )
-            # normalize coordinate difference
-            graph.edata["x_diff"] = graph.edata["x_diff"] / (
-                graph.edata["radial"].sqrt() + 1e-30
-            )
 
-            # compute messages along every edge
-            graph.apply_edges(self.message)
+            # compute euclidean distance across all edges
+            for etype in graph.etypes:
+                graph.apply_edges(
+                    lambda edges: {'dij': 
+                    torch.linalg.vector_norm(edges.data['x_diff'], dim=1).unsqueeze(-1)},
+                    etype=etype)
 
-            # aggregate messages for both coordinates and features
-            graph.update_all(fn.copy_e("msg_x", "m"), fn.mean("m", "x_neigh"))
+
+            # normalize displacement vectors to unit length
+            for etype in graph.etypes:
+                graph.apply_edges(
+                    lambda edges: {'x_diff': edges.data['x_diff'] / (edges.data['dij'] + 1e-9)},
+                    etype=etype)
+
+
+            # compute messages and store them on every edge
+            for etype in graph.etypes:
+                graph.apply_edges(self.message, etype=etype)
+
+            # aggregating messages from all edges
+            graph.update_all(fn.copy_e("msg_x", "m"), fn.sum("m", "x_neigh"))
             graph.update_all(fn.copy_e("msg_h", "m"), fn.sum("m", "h_neigh"))
 
+
+            # get aggregated messages
             h_neigh, x_neigh = graph.ndata["h_neigh"], graph.ndata["x_neigh"]
 
-            # compute updated coordinates and features
-            h = self.node_mlp(torch.cat([node_feat, h_neigh], dim=-1))
-            x = coord_feat + x_neigh
+
+            # compute updated features/coordinates
+            # note that the receptor features/coordinates are not updated
+            node_mlp_input = torch.cat([node_feat['lig'], h_neigh['lig']], dim=-1)
+            h = {'lig': node_feat['lig'] + self.node_mlp(node_mlp_input),
+                 'rec': node_feat['rec']}
+
+
+            x = {'lig': coord_feat['lig'] + x_neigh['lig'],
+                 'rec': coord_feat['rec']}
 
             return h, x
+
