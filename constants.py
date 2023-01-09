5e4d39837c20a1cfdb0f9d3f3696392e1dd99cd2
Ian Dunn <ian@iandunn.io>
2023-01-09 16:13:13 -0500

    added constants file

---
+++ constants.py
@@ -0,0 +1,6 @@
+
+# this is taken from DiffSBDD
+allowed_bonds = {'H': 1, 'C': 4, 'N': 3, 'O': 2, 'F': 1, 'B': 3, 'Al': 3,
+                 'Si': 4, 'P': [3, 5],
+                 'S': 4, 'Cl': 1, 'As': 3, 'Br': 1, 'I': 1, 'Hg': [1, 2],
+                 'Bi': [3, 5]}
