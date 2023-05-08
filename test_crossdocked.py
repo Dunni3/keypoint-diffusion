3c36d2635d307e8c80618699abd05808a402b19d
Ian Dunn <ian@iandunn.io>
2023-05-08 11:22:58 -0400

    fixed testing code for datasets that have interface poitns

--- test_crossdocked.py
+++ test_crossdocked.py
@@ -165,7 +165,7 @@ def main():
         pocket_sample_start = time.time()
 
         # get receptor graph and reference ligand positions/features from test set
-        rec_graph, ref_lig_pos, ref_lig_feat = test_dataset[dataset_idx]
+        rec_graph, ref_lig_pos, ref_lig_feat, _ = test_dataset[dataset_idx]
         rec_file_path, ref_lig_file_path = test_dataset.get_files(dataset_idx) # get original rec/lig files
 
         # move data to gpu
