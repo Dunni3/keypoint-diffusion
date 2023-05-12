4f07c0300091b28f886c57ca6a0163edec242870
Ian Dunn <ian@iandunn.io>
2023-05-12 13:04:13 -0400

    deleting unused code

--- process_bindingmoad.py
+++ process_bindingmoad.py
@@ -26,11 +26,6 @@ import pickle
 from data_processing.pdbbind_processing import rec_atom_featurizer, lig_atom_featurizer, Unparsable, build_receptor_graph
 from utils import get_rec_atom_map
 
-# dataset_info = dataset_params['bindingmoad']
-# amino_acid_dict = dataset_info['aa_encoder']
-# atom_dict = dataset_info['atom_encoder']
-# atom_decoder = dataset_info['atom_decoder']
-
 
 def element_fixer(element: str):
 
@@ -77,210 +72,13 @@ def read_label_file(csv_path):
 
     return ligand_dict
 
-
-def compute_druglikeness(ligand_dict):
-    """
-    Computes RDKit's QED value and adds it to the dictionary
-    Args:
-        ligand_dict: nested ligand dictionary
-    Returns:
-        the same ligand dictionary with additional QED values
-    """
-    print("Computing QED values...")
-    for p, m in tqdm([(p, m) for c in ligand_dict for p in ligand_dict[c]
-                      for m in ligand_dict[c][p]]):
-        mol = Chem.MolFromSmiles(m[2])
-        if mol is None:
-            mol_id = f'{p}_{m}'
-            warnings.warn(f"Could not construct molecule {mol_id} from SMILES "
-                          f"string '{m[2]}'")
-            continue
-        m.append(QED.qed(mol))
-    return ligand_dict
-
-
-def filter_and_flatten(ligand_dict, qed_thresh, max_occurences, seed):
-
-    filtered_examples = []
-    all_examples = [(c, p, m) for c in ligand_dict for p in ligand_dict[c]
-                    for m in ligand_dict[c][p]]
-
-    # shuffle to select random examples of ligands that occur more than
-    # max_occurences times
-    random.seed(seed)
-    random.shuffle(all_examples)
-
-    ligand_name_counter = defaultdict(int)
-    print("Filtering examples...")
-    for c, p, m in tqdm(all_examples):
-
-        ligand_name, ligand_chain, ligand_resi = m[0].split(':')
-        if m[1] == 'valid' and len(m) > 3 and m[3] > qed_thresh:
-            if ligand_name_counter[ligand_name] < max_occurences:
-                filtered_examples.append(
-                    (c, p, m)
-                )
-                ligand_name_counter[ligand_name] += 1
-
-    return filtered_examples
-
-
-def split_by_ec_number(data_list, n_val, n_test, ec_level=1):
-    """
-    Split dataset into training, validation and test sets based on EC numbers
-    https://en.wikipedia.org/wiki/Enzyme_Commission_number
-    Args:
-        data_list: list of ligands
-        n_val: number of validation examples
-        n_test: number of test examples
-        ec_level: level in the EC numbering hierarchy at which the split is
-            made, i.e. items with matching EC numbers at this level are put in
-            the same set
-    Returns:
-        dictionary with keys 'train', 'val', and 'test'
-    """
-
-    examples_per_class = defaultdict(int)
-    for c, p, m in data_list:
-        c_sub = '.'.join(c.split('.')[:ec_level])
-        examples_per_class[c_sub] += 1
-
-    assert sum(examples_per_class.values()) == len(data_list)
-
-    # split ec numbers
-    val_classes = set()
-    for c, num in sorted(examples_per_class.items(), key=lambda x: x[1],
-                         reverse=True):
-        if sum([examples_per_class[x] for x in val_classes]) + num <= n_val:
-            val_classes.add(c)
-
-    test_classes = set()
-    for c, num in sorted(examples_per_class.items(), key=lambda x: x[1],
-                         reverse=True):
-        # skip classes already used in the validation set
-        if c in val_classes:
-            continue
-        if sum([examples_per_class[x] for x in test_classes]) + num <= n_test:
-            test_classes.add(c)
-
-    # remaining classes belong to test set
-    train_classes = {x for x in examples_per_class if
-                     x not in val_classes and x not in test_classes}
-
-    # create separate lists of examples
-    data_split = {}
-    data_split['train'] = [x for x in data_list if '.'.join(
-        x[0].split('.')[:ec_level]) in train_classes]
-    data_split['val'] = [x for x in data_list if '.'.join(
-        x[0].split('.')[:ec_level]) in val_classes]
-    data_split['test'] = [x for x in data_list if '.'.join(
-        x[0].split('.')[:ec_level]) in test_classes]
-
-    assert len(data_split['train']) + len(data_split['val']) + \
-           len(data_split['test']) == len(data_list)
-
-    return data_split
-
-
 def ligand_list_to_dict(ligand_list):
     out_dict = defaultdict(list)
     for _, p, m in ligand_list:
         out_dict[p].append(m)
     return out_dict
 
-
-def process_ligand_and_pocket(pdb_struct, ligand_name, ligand_chain,
-                              ligand_resi, dist_cutoff, ca_only,
-                              compute_quaternion=False):
-    try:
-        residues = {obj.id[1]: obj for obj in
-                    pdb_struct[0][ligand_chain].get_residues()}
-    except KeyError as e:
-        raise KeyError(f'Chain {e} not found ({pdbfile}, '
-                       f'{ligand_name}:{ligand_chain}:{ligand_resi})')
-    ligand = residues[ligand_resi]
-    assert ligand.get_resname() == ligand_name, \
-        f"{ligand.get_resname()} != {ligand_name}"
-
-    # remove H atoms if not in atom_dict, other atom types that aren't allowed
-    # should stay so that the entire ligand can be removed from the dataset
-    lig_atoms = [a for a in ligand.get_atoms()
-                 if (a.element.capitalize() in atom_dict or a.element != 'H')]
-    lig_coords = np.array([a.get_coord() for a in lig_atoms])
-
-    try:
-        lig_one_hot = np.stack([
-            np.eye(1, len(atom_dict), atom_dict[a.element.capitalize()]).squeeze()
-            for a in lig_atoms
-        ])
-    except KeyError as e:
-        raise KeyError(
-            f'Ligand atom {e} not in atom dict ({pdbfile}, '
-            f'{ligand_name}:{ligand_chain}:{ligand_resi})')
-
-    # Find interacting pocket residues based on distance cutoff
-    pocket_residues = []
-    for residue in pdb_struct[0].get_residues():
-        res_coords = np.array([a.get_coord() for a in residue.get_atoms()])
-        if is_aa(residue.get_resname(), standard=True) and \
-                (((res_coords[:, None, :] - lig_coords[None, :, :]) ** 2).sum(-1) ** 0.5).min() < dist_cutoff:
-            pocket_residues.append(residue)
-
-    # Compute transform of the canonical reference frame
-    n_xyz = np.array([res['N'].get_coord() for res in pocket_residues])
-    ca_xyz = np.array([res['CA'].get_coord() for res in pocket_residues])
-    c_xyz = np.array([res['C'].get_coord() for res in pocket_residues])
-
-    if compute_quaternion:
-        quaternion, c_alpha = get_bb_transform(n_xyz, ca_xyz, c_xyz)
-        if np.any(np.isnan(quaternion)):
-            raise ValueError(
-                f'Invalid value in quaternion ({pdbfile}, '
-                f'{ligand_name}:{ligand_chain}:{ligand_resi})')
-    else:
-        c_alpha = ca_xyz
-
-    if ca_only:
-        pocket_coords = c_alpha
-        try:
-            pocket_one_hot = np.stack([
-                np.eye(1, len(amino_acid_dict),
-                       amino_acid_dict[three_to_one(res.get_resname())]).squeeze()
-                for res in pocket_residues])
-        except KeyError as e:
-            raise KeyError(
-                f'{e} not in amino acid dict ({pdbfile}, '
-                f'{ligand_name}:{ligand_chain}:{ligand_resi})')
-    else:
-        pocket_atoms = [a for res in pocket_residues for a in res.get_atoms()
-                        if (a.element.capitalize() in atom_dict or a.element != 'H')]
-        pocket_coords = np.array([a.get_coord() for a in pocket_atoms])
-        try:
-            pocket_one_hot = np.stack([
-                np.eye(1, len(atom_dict), atom_dict[a.element.capitalize()]).squeeze()
-                for a in pocket_atoms
-            ])
-        except KeyError as e:
-            raise KeyError(
-                f'Pocket atom {e} not in atom dict ({pdbfile}, '
-                f'{ligand_name}:{ligand_chain}:{ligand_resi})')
-
-    pocket_ids = [f'{res.parent.id}:{res.id[1]}' for res in pocket_residues]
-
-    ligand_data = {
-        'lig_coords': lig_coords,
-        'lig_one_hot': lig_one_hot,
-    }
-    pocket_data = {
-        'pocket_ca': pocket_coords,
-        'pocket_one_hot': pocket_one_hot,
-        'pocket_ids': pocket_ids,
-    }
-    if compute_quaternion:
-        pocket_data['pocket_quaternion'] = quaternion
-    return ligand_data, pocket_data
-
-def process_ligand_and_pocket_new(pdb_struct, ligand_name, ligand_chain, ligand_resi,
+def process_ligand_and_pocket(pdb_struct, ligand_name, ligand_chain, ligand_resi,
                                   rec_element_map, lig_element_map,
                                   receptor_k: int, pocket_edge_algorithm: str, 
                                   dist_cutoff: float, remove_hydrogen: bool = True):
@@ -478,7 +276,7 @@ if __name__ == '__main__':
                         ligand_resi = int(ligand_resi)
 
                         try:
-                            rec_graph, lig_atom_positions, lig_atom_features = process_ligand_and_pocket_new(pdb_struct, 
+                            rec_graph, lig_atom_positions, lig_atom_features = process_ligand_and_pocket(pdb_struct, 
                                                         ligand_name, 
                                                         ligand_chain, 
                                                         ligand_resi,
