c606d148116acd3f4f0139a447bf8df6231b2d0e
Ian Dunn <ian@iandunn.io>
2023-11-08 13:03:48 -0500

    some commands for sampling

--- gen_test_commands.py
+++ gen_test_commands.py
@@ -8,12 +8,14 @@ def parse_arguments():
     p.add_argument('--dataset_idxs_file', type=Path, default=Path('val_subset/val_idxs.pkl'))
     p.add_argument('--output_cmd_file', type=Path, default=Path('test_cmds_parallel.txt'))
     p.add_argument('--lines', type=int, nargs='+', default=[])
+    p.add_argument('--filenames_file', type=Path, default=None)
     args = p.parse_args()
     return args
 
 if __name__ == "__main__":
 
     args = parse_arguments()
+
     with open(args.base_cmd_file, 'r') as f:
         base_cmd_lines = [ line.strip() for line in f ]
 
@@ -22,8 +24,13 @@ if __name__ == "__main__":
 
     base_cmd_lines = [ x for i,x in enumerate(base_cmd_lines) if i in args.lines ]
 
-    with open(args.dataset_idxs_file, 'rb') as f:
-        dataset_idxs = pickle.load(f)
+    if args.filenames_file is not None:
+        with open(args.filenames_file, 'rb') as f:
+            filenames_dict = pickle.load(f)
+        dataset_idxs = list(range(len(filenames_dict['lig_files'])))
+    else:
+        with open(args.dataset_idxs_file, 'rb') as f:
+            dataset_idxs = pickle.load(f)
     
     cmds = []
     for base_cmd in base_cmd_lines:
